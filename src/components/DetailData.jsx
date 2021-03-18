ac8dd03e33a672e1c8a815b0725ef768b1996d93
DenesVR <denesvanroy@gmail.com>
2021-03-18 10:50:46 +0100

    Klaar

--- src/components/DetailData.jsx
+++ src/components/DetailData.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function DetailData({ btw: [{ eub_land, eub_code }] }) {
+function DetailData({ btw: [{ eub_id, eub_land, eub_code }] }) {
   return (
     <div className="detail">
+      <p>ID: {eub_id}</p>
       <p>Land: {eub_land}</p>
       <p>Code: {eub_code}</p>
     </div>
