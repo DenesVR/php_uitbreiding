f305508fda800394c5d59400f1c5588f26875e80
DenesVR <denesvanroy@gmail.com>
2021-03-18 11:07:16 +0100

    Klaar

--- src/components/Btw.jsx
+++ src/components/Btw.jsx
@@ -3,10 +3,6 @@ import { Link } from "react-router-dom";
 
 function Btw({ btwcodes }) {
   return (
-    // <Link to={`/btw/${btwcodes.eub_id}`}>
-    /* <p>
-        <pre>{JSON.stringify(btwcodes)}</pre>
-      </p> */
     <ul>
       {btwcodes.map(btwcode => (
         <Link to={`/btw/${btwcode.eub_id}`}>
